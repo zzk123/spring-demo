7137a6f4468d0d6bd2920d441a1175e7df50dfa1
程序猿 <753809163@qq.com>
2023-05-14 11:10:59 +0800

    add

---
+++ eureka-client/src/main/java/com/zzk/eurekaclient/Application.java
@@ -0,0 +1,10 @@
+package com.zzk.eurekaclient;
+
+/**
+ * @ClassName com.zzk.eurekaclient.Application
+ * @Description TODO
+ * @Author zzk
+ * @Date 2023/4/30 22:33
+ **/
+public class Application {
+}
